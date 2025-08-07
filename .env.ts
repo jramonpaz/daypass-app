6f685b8f2fa2d50d4aedeef04a00e9e83e3c29cf
Jose Ramon Paz <jramon.paz@tufabricadeventos.com>
2025-08-07 19:20:08 +0200

    Aliminador del repo fichero que no deben de estar

--- .env.ts
+++
@@ -1,7 +0,0 @@
-// # can be a js/ts file
-export const STAGE = 'production'; // 'production' || 'development'
-export const BASE_API_URL_DEV = 'https://www.daypass.com:8855';
-export const BASE_API_URL_PROD = 'https://api.daypass.com';
-export const STRIPE_PUBLISHABLE_KEY = 'asdkjwqib q qwhuiqekjqwe qwiehqwehk';
-export const AUTH0_DOMAIN = 'asda'
-export const AUTH0_CLIENT_ID = 'asdaasd'
